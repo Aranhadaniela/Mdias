29eda984f97509d2ff5a0db90d27e4634ba79631
iana <ianajabami94@gmail.com>
2026-05-29 20:53:58 -0300

    get adicionado

--- app/routes/donation_routes.py
+++ app/routes/donation_routes.py
@@ -1,6 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+
 from app.schemas.donation_schema import DonationFormRequest
-from app.services.donation_service import create_donation_form
+from app.services.donation_service import (
+    create_donation_form,
+    list_donation_forms,
+    get_donation_form_by_id
+)
 
 router = APIRouter(
     tags=["Formulário de Doação"]
@@ -14,5 +19,32 @@ def submit_donation_form(data: DonationFormRequest):
     return {
         "success": True,
         "message": "Formulário de doação enviado com sucesso.",
+        "data": result
+    }
+
+
+@router.get("/formulario-doacao")
+def get_donation_forms():
+    result = list_donation_forms()
+
+    return {
+        "success": True,
+        "total": len(result),
+        "data": result
+    }
+
+
+@router.get("/formulario-doacao/{donation_id}")
+def get_donation_form(donation_id: int):
+    result = get_donation_form_by_id(donation_id)
+
+    if not result:
+        raise HTTPException(
+            status_code=404,
+            detail="Solicitação de doação não encontrada."
+        )
+
+    return {
+        "success": True,
         "data": result
     }
--- app/services/donation_service.py
+++ app/services/donation_service.py
@@ -1,14 +1,31 @@
 from datetime import datetime
 from app.schemas.donation_schema import DonationFormRequest
 
+donation_forms = []
+
 
 def create_donation_form(data: DonationFormRequest):
     donation_data = data.model_dump()
 
+    donation_data["id"] = len(donation_forms) + 1
     donation_data["status"] = "recebido"
     donation_data["criado_em"] = datetime.now().isoformat()
+
+    donation_forms.append(donation_data)
 
     print("Nova solicitação de doação recebida:")
     print(donation_data)
 
     return donation_data
+
+
+def list_donation_forms():
+    return donation_forms
+
+
+def get_donation_form_by_id(donation_id: int):
+    for donation in donation_forms:
+        if donation["id"] == donation_id:
+            return donation
+
+    return None
